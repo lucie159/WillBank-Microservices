4f18cf5f392569b0aa3db5806d1355ee6c911a42
lucie159 <luciesaintpierreedene@gmail.com>
2025-12-29 09:06:50 +0100

    Initial commit: Infrastructure Docker + Eureka + Gateway

---
+++ gateway-service/gateway-service/src/test/java/com/willbank/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willbank.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
